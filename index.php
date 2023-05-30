63f38f6e2ded3368136c07a0caa3a92ab417d305
Brooklyn <101115866+Brooklyn2309@users.noreply.github.com>
2023-05-30 12:35:01 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,5 +2,5 @@
 
 echo "<a href='insertKlant.php'>Voeg klant toe</a></br>";
 echo "<a href='insertVerkoop.php'>Voeg verkooporders toe</a></br>";
-
+echo "<a href='selectVerkoop.php'>Laat verkooporders zien</a></br>";
 ?>
